846b1de42c7eecc433741bc3cd58f0778138425a
HactorMC <44316275+HactorMC@users.noreply.github.com>
2018-12-01 13:09:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '!'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`SOON  `,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`SOON  `,"idle")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
