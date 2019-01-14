22fd1ee897c84ccff8163d3f550b61ce48d1b083
HactorMC <44316275+HactorMC@users.noreply.github.com>
2019-01-14 11:16:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -957,3 +957,13 @@ client.on("message", (message) => {
     }
  
 });
+
+client.on('message', message => {
+
+  if (message.content.startsWith( prefix + "rep")) {
+  if (!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1).join(' ');
+message.channel.send(
+      "\n" + "#rep <@!" + args + ">")
+  }
+});
