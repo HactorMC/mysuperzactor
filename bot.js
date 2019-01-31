f55d7bdbc225dade17533e91cd72d850aabaa461
HactorMC <44316275+HactorMC@users.noreply.github.com>
2019-01-31 12:56:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1936,21 +1936,3 @@ if (message.content.startsWith("*cv")) {
             message.channel.send(Math.floor(Math.random() * args))
         }
     });
-
-client.on("message", async (message) => {
- if (message.author.bot) return;
- if (!message.content.startsWith(prefix)) return;
-    
- let command = message.content.split(" ")[0];
- command = command.slice(prefix.length);
-    
- let args = message.content.split(" ").slice(1);
-    
- if (command === "say") {
-     message.delete()
-     const embed = new Discord.RichEmbed()
-     .setColor(0x954D23)
-     .setDescription(message.author.username + " says: " + args.join(" "));
-     message.channel.send({embed})
-    }
-});
