ce34a78480cef81d0b870a165083aacce353ed09
HactorMC <44316275+HactorMC@users.noreply.github.com>
2019-01-31 20:46:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,32 @@ const prefix = '!'
  
  
 client.on('ready', () => {
-        console.log(`back`);
-              client.user.setActivity("!help | !inv", {type: 'WATCHING'});
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`!help | !inv  `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
  
-
+ 
  
 client.login(process.env.BOT_TOKEN);
 
